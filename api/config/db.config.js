45650bcbd4faa3d7018fb5b6a82103c2b3a7c19a
BRAINFREEZER1999 <141788556+BRAINFREEZER1999@users.noreply.github.com>
2023-10-16 03:02:17 +1100

    Update db.config.js

--- api/config/db.config.js
+++ api/config/db.config.js
@@ -2,10 +2,10 @@ const DATABASE_URL = process.env.DATABASE_URL || '';
 const [_, username, password, host, port, dbname] = DATABASE_URL.match(/postgres:\/\/(.*):(.*?)@(.*?):(.*?)\/(.*)/) || [];
 
 module.exports = {
-    HOST: "db",
-    USER: "postgres",
-    PASSWORD: "postgres",
-    DB: "postgres",
+    HOST: host || "db",
+    USER: username || "postgres",
+    PASSWORD: password || "postgres",
+    DB: dbname || "postgres",
     dialect: "postgres",
     pool: {
       max: 5,
