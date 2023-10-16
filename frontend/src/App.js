bee583f3fd6e9e1d4bccc8dcea4aaa1fe688dc01
BRAINFREEZER1999 <141788556+BRAINFREEZER1999@users.noreply.github.com>
2023-10-17 03:01:58 +1100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,17 @@
-import { useState, useEffect } from 'react';  // import useEffect
+
+
+
+
+import React from 'react'; // Ensure you import React
+import { useState, useEffect } from 'react';  
 import './App.css';
+import ContactList from './ContactList';  // import ContactList component
 
 function App() {
-
     return (
         <div>     
             <h1>Contactor</h1>
+            <ContactList />   // include the ContactList component here
         </div>
     );
 }
