5e43084c430fed297c5121b754b3ba4d3944a07e
BRAINFREEZER1999 <141788556+BRAINFREEZER1999@users.noreply.github.com>
2023-10-16 14:36:07 +1100

    Update contact.model.js

--- api/models/contact.model.js
+++ api/models/contact.model.js
@@ -1,12 +1,23 @@
 module.exports = (sequelize, Sequelize) => {
-    const Contact = sequelize.define("contact", {
+    const Contact = sequelize.define("Contact", {  
         id: {
             type: Sequelize.INTEGER,
             autoIncrement: true,
             primaryKey: true,
         },
-        // DEFINE YOUR MODEL HERE
+        name: {
+            type: Sequelize.STRING,
+            allowNull: false
+        }
     });
   
+    // Association (will be set up later in the flow)
+    Contact.associate = (models) => {
+        Contact.hasMany(models.Phone, {
+            foreignKey: 'contactId',
+            as: 'phones'
+        });
+    };
+
     return Contact;
 };
