bf472dfbd88dbbd4c290bc85de330abda646003b
BRAINFREEZER1999 <141788556+BRAINFREEZER1999@users.noreply.github.com>
2023-10-18 02:27:08 +1100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,9 @@
-
-
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import ContactList from './ContactList';
 import ContactDetail from './ContactDetail';
+import AddContact from './AddContact'; // Import this
+import AddPhoneNumber from './AddPhoneNumber'; // And this
 
 function App() {
     return (
@@ -12,6 +12,8 @@ function App() {
                 <Switch>
                     <Route path="/" exact component={ContactList} />
                     <Route path="/contact/:contactId" component={ContactDetail} />
+                    <Route path="/add-contact" component={AddContact} /> // New route
+                    <Route path="/contact/:contactId/add-phone" component={AddPhoneNumber} /> // New route
                 </Switch>
             </div>
         </Router>
