1b42bf6536d938ce22b60fe3dc9705e4b3626c4d
BRAINFREEZER1999 <141788556+BRAINFREEZER1999@users.noreply.github.com>
2023-10-18 03:00:23 +1100

    Update stats.controller.js

--- api/controllers/stats.controller.js
+++ api/controllers/stats.controller.js
@@ -1,9 +1,32 @@
 const db = require("../models");
-const Phones = db.phones;
-const Contacts = db.contacts;
-const Op = db.Sequelize.Op;
+const Contact = db.contacts;
+const Phone = db.phones;
 
-// Calculate stats
-exports.calculate = (req, res) => {
-    
+exports.stats = (req, res) => {
+    // Get total number of contacts
+    const contactCount = Contact.count();
+
+    // Get total number of phone numbers
+    const phoneCount = Phone.count();
+
+    // Get the creation time of the most recent contact
+    const mostRecentContactTime = Contact.max('createdAt');
+
+    // Get the creation time of the oldest contact
+    const oldestContactTime = Contact.min('createdAt');
+
+    Promise.all([contactCount, phoneCount, mostRecentContactTime, oldestContactTime])
+        .then(results => {
+            res.send({
+                totalContacts: results[0],
+                totalPhones: results[1],
+                mostRecentContactTime: results[2],
+                oldestContactTime: results[3]
+            });
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: err.message || "Some error occurred while retrieving stats."
+            });
+        });
 };
