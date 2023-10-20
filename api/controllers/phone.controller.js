a578d9e33d0f4b6393fc5d3dabde4f88315ab6c6
BRAINFREEZER1999 <141788556+BRAINFREEZER1999@users.noreply.github.com>
2023-10-20 22:42:55 +1100

    Update phone.controller.js

--- api/controllers/phone.controller.js
+++ api/controllers/phone.controller.js
@@ -2,27 +2,22 @@ const db = require("../models");
 const Phones = db.phones;
 const Op = db.Sequelize.Op;
 
-// Create phone
 exports.create = (req, res) => {
     
 };
 
-// Get all phones
 exports.findAll = (req, res) => {
     
 };
 
-// Get one phone by id
 exports.findOne = (req, res) => {
   
 };
 
-// Update one phone by id
 exports.update = (req, res) => {
     
 };
 
-// Delete one phone by id
 exports.delete = (req, res) => {
     
 };
