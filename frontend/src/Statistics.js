0f2fa6ce69c705e6aa8e55b01de86aacb027f0fa
BRAINFREEZER1999 <141788556+BRAINFREEZER1999@users.noreply.github.com>
2023-10-20 16:31:37 +1100

    Update Statistics.js

--- frontend/src/Statistics.js
+++ frontend/src/Statistics.js
@@ -1,15 +1,17 @@
-// Statistics.js
-
 import React, { useState, useEffect } from 'react';
 
 function Statistics() {
     const [stats, setStats] = useState(null);
 
-    useEffect(() => {
+    const fetchStatistics = () => {
         fetch('http://localhost:5000/api/stats')
             .then(response => response.json())
             .then(data => setStats(data))
             .catch(error => console.error('Error fetching statistics:', error));
+    }
+
+    useEffect(() => {
+        fetchStatistics(); // Call the function on component mount
     }, []);
 
     if (!stats) {
@@ -25,6 +27,7 @@ function Statistics() {
                 <li>Most recent contact time: {new Date(stats.mostRecentContactTime).toLocaleString()}</li>
                 <li>Oldest contact creation time: {new Date(stats.oldestContactTime).toLocaleString()}</li>
             </ul>
+            <button onClick={fetchStatistics}>Refresh</button> {/* This is the refresh button */}
         </div>
     );
 }
